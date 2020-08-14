4dfdbc8a47515ccfbb2f5956bde1d6fcb371edaa
Aurelia Bălășoiu <aureliabalasoiu@gmail.com>
2020-08-14 15:00:51 +0300

    Added "Logout" functionality

---
+++ TeacherEvaluation/TeacherEvaluation.Application/Pages/Account/Logout.cshtml.cs
@@ -0,0 +1,28 @@
+﻿using Microsoft.AspNetCore.Identity;
+using Microsoft.AspNetCore.Mvc;
+using Microsoft.AspNetCore.Mvc.RazorPages;
+using System.Threading.Tasks;
+using TeacherEvaluation.Domain.Identity;
+
+namespace TeacherEvaluation.Application.Pages.Account
+{
+    public class LogoutModel : PageModel
+    {
+        private readonly SignInManager<ApplicationUser> signInManager;
+
+        public LogoutModel(SignInManager<ApplicationUser> signInManager)
+        {
+            this.signInManager = signInManager;
+        }
+
+        public void OnGet()
+        {
+        }
+
+        public async Task<IActionResult> OnPost()
+        {
+            await signInManager.SignOutAsync();
+            return RedirectToPage("/Account/Login");
+        }
+    }
+}
