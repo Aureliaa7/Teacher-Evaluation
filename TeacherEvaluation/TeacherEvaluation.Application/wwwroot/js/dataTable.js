e0a61016412d35be0d9df883548753b6a1485198
Aurelia Bălășoiu <aureliabalasoiu@gmail.com>
2020-09-09 10:13:00 +0300

    Solved "sorting arrows not showing for students and enrollments" bug

--- TeacherEvaluation/TeacherEvaluation.Application/wwwroot/js/dataTable.js
+++ TeacherEvaluation/TeacherEvaluation.Application/wwwroot/js/dataTable.js
@@ -1,3 +1,15 @@
 ﻿$(document).ready(function () {
     $('#my-table').DataTable();
 });
+
+$(document).ready(function () {
+    $('#students-index').DataTable({
+        'columnDefs': [{ 'orderable': false, 'targets': 6 }]
+    });
+});
+
+$(document).ready(function () {
+    $('#enrollments-index').DataTable({
+        'columnDefs': [{ 'orderable': false, 'targets': 9 }]
+    });
+});
