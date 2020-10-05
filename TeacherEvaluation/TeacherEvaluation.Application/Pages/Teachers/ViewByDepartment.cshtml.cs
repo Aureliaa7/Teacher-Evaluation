09983a9f94c3cbaec538b105658c8105dbafb710
Aurelia Bălășoiu <aureliabalasoiu@gmail.com>
2020-10-05 13:00:35 +0300

    Added "View Teachers by Department" functionality

---
+++ TeacherEvaluation/TeacherEvaluation.Application/Pages/Teachers/ViewByDepartment.cshtml.cs
@@ -0,0 +1,41 @@
+using System.Collections.Generic;
+using System.ComponentModel.DataAnnotations;
+using System.Threading.Tasks;
+using MediatR;
+using Microsoft.AspNetCore.Mvc;
+using Microsoft.AspNetCore.Mvc.RazorPages;
+using TeacherEvaluation.BusinessLogic.Commands.Teachers.CrudOperations;
+using TeacherEvaluation.Domain.DomainEntities;
+
+namespace TeacherEvaluation.Application.Pages.Teachers
+{
+    public class ViewByDepartmentModel : PageModel
+    {
+        private readonly IMediator mediator;
+        public bool TableIsVisible;
+
+        [BindProperty]
+        [EnumDataType(typeof(Department))]
+        public Department Department { get; set; }
+
+        [BindProperty]
+        public IEnumerable<Teacher> Teachers { get; set; }
+
+        public ViewByDepartmentModel(IMediator mediator)
+        {
+            this.mediator = mediator;
+            Teachers = new List<Teacher>();
+        }
+
+        public void OnGet()
+        {
+        }
+
+        public async Task OnPostAsync()
+        {
+            GetTeachersByDepartmentCommand command = new GetTeachersByDepartmentCommand { Department = Department };
+            Teachers = await mediator.Send(command);
+            TableIsVisible = true;
+        }
+    }
+}
