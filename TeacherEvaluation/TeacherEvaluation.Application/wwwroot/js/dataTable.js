b2aa2975812433f64b44a8222207f1c873e977a7
Aurelia Bălășoiu <aureliabalasoiu@gmail.com>
2021-03-26 21:05:07 +0200

    Repleced Delete, Details and Edit with specific icons

--- TeacherEvaluation/TeacherEvaluation.Application/wwwroot/js/dataTable.js
+++ TeacherEvaluation/TeacherEvaluation.Application/wwwroot/js/dataTable.js
@@ -28,7 +28,7 @@ $(document).ready(function () {
 
 $(document).ready(function () {
     $('#subjects-index').DataTable({
-        'columnDefs': [{ 'orderable': false, 'targets': 2 }]
+        'columnDefs': [{ 'orderable': false, 'targets': 6 }]
     });
 });
 
