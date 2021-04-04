09fa7cb4c5323139e2bd6f8ac50b964c37121a4b
Aurelia Bălășoiu <aureliabalasoiu@gmail.com>
2021-04-04 09:13:27 +0300

    Fixed draw charts bug

--- TeacherEvaluation/TeacherEvaluation.Application/wwwroot/js/charts.js
+++ TeacherEvaluation/TeacherEvaluation.Application/wwwroot/js/charts.js
@@ -13,12 +13,47 @@
         url: "/EvaluationForms/ViewResponses?handler=RetrieveResponses",
 
         success: function (result) {
-            google.charts.load("current", { packages: ["corechart"] });
-            var contor = 1;
-            for (var key in result) {
-                google.charts.setOnLoadCallback(function () { draw_charts(key, result[key], contor); });
-                contor++;
-            }
+            google.charts.load('current', {
+                callback: function () {
+                    var data = [];
+                    var options = [];
+                    var divIds = [];
+
+                    // prepare the data for charts
+                    var contor = 0;
+                    for (var key in result) {
+                        data[contor] = new google.visualization.DataTable();
+                        data[contor].addColumn('string', 'question');
+                        data[contor].addColumn('number', 'number of answers');
+                        data[contor].addRows(6);
+
+                        var contor2 = 0;
+                        var dictionary = result[key]
+                        for (var key2 in dictionary) {
+                            var numberOfAnswers = dictionary[key2];
+                            data[contor].setCell(contor2, 0, key2);
+                            data[contor].setCell(contor2, 1, numberOfAnswers);
+                            contor2++;
+                        }
+
+                        options[contor] = {
+                            title: key, // this is the question
+                            is3D: true,
+                        };
+                        divIds[contor] = "question" + (contor + 1);
+
+                        contor++;
+
+                    }
+
+                    // draw the charts
+                    for (var contor = 0; contor < 10; contor++) {
+                        var chart = new google.visualization.PieChart(document.getElementById(divIds[contor]));
+                        chart.draw(data[contor], options[contor]);
+                    }
+                },
+                packages: ['corechart']
+            });
 
             console.log("*** responses: ", result);
         },
@@ -26,29 +61,4 @@
             console.log("Something went wrong");
         }
     });
-}
-
-function draw_charts(questionText, dictionary, contor) {
-    var data = new google.visualization.DataTable();
-    data.addColumn('string', 'question');
-    data.addColumn('number', 'number of answers');
-    data.addRows(6);
-   
-    var contor = 0; 
-    for (var key in dictionary) {
-        var numberOfAnswers = dictionary[key];
-        data.setCell(contor, 0, key);
-        data.setCell(contor, 1, numberOfAnswers);
-        contor++;
-    }
-
-    var options = {
-        title: questionText,
-        is3D: true,
-    };
-
-    var id = 'question'.concat(contor.toString());
-    console.log("id: ", id);
-    var chart = new google.visualization.PieChart(document.getElementById(id));
-    chart.draw(data, options);
 }
